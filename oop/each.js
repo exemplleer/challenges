fb8e40c5040aac0ea0751aa9f27a918dd483b20a
maksim.av <exemplleer@gmail.com>
2023-04-19 22:51:58 +0300

    add new solution

---
+++ oop/each.js
@@ -0,0 +1,20 @@
+/* 
+Реализуйте и экспортируйте функцию по умолчанию, которая принимает на вход
+коллекцию объектов и колбек функцию. Эта функция должна вызывать колбек для
+каждого объекта коллекции. Главная особенность этой функции в том, что она
+передает объекты коллекции не в саму функцию, а устанавливает их как контекст.
+
+Примеры:
+const objects = [
+  { name: 'Karl' },
+  { name: 'Mia' },
+];
+each(objects, function callback() {
+  this.name = this.name.split('').reverse().join('');
+});
+console.log(objects); [{ name: 'lraK' }, { name: 'aiM' }];
+*/
+
+const each = (coll, callbackfn) => coll.forEach((item) => callbackfn.call(item));
+
+export default each;
