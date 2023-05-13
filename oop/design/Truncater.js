0c0e1b992027e1ad04ec37f745ddaaec3a47ce33
maksim.av <exemplleer@gmail.com>
2023-05-13 14:47:13 +0300

    add new solution

---
+++ oop/design/Truncater.js
@@ -0,0 +1,39 @@
+/* 
+Реализуйте в классе Truncater конструктор и метод truncate(). Метод принимает
+текст и следующие опции:
+- separator - символ, заменяющий обрезанную часть строки
+- length - максимальная длина исходной строки. Если строка не длинне, чем эта
+  опция, то возвращается исходная строка.
+Конфигурацию по умолчанию можно переопределить через конструктор класса и
+вторым аргументом метода truncate(). Оба способа можно комбинировать.
+
+Примеры:
+const truncater = new Truncater();
+truncater.truncate('one two'); // 'one two'
+truncater.truncate('one two', { 'length': 6 }); // 'one tw...'
+
+const truncater = new Truncater({ 'length': 6 });
+truncater.truncate('one two', { 'separator': '.' }); // 'one tw.'
+truncater.truncate('one two', { 'length': 3 }); // 'one...'
+*/
+
+export default class Truncater {
+  static defaultOptions = {
+    separator: '...',
+    length: 200,
+  };
+
+  constructor(options = {}) {
+    this.options = { ...this.constructor.defaultOptions, ...options };
+  }
+
+  truncate(text, options = {}) {
+    const { length, separator } = { ...this.options, ...options };
+
+    if (text.length <= length) {
+      return text;
+    }
+
+    return `${text.substr(0, length)}${separator}`;
+  }
+}
